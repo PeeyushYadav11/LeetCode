0fa94a88658b897fb2497e0304d2d89ea45e9696
Peeyush Yadav <peeyushyadav11122005@gmail.com>
2026-08-05 19:44:57 +0530

    Time: 50 ms (95.19%), Space: 283.9 MB (59.62%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+
+        List<List<Integer>> adjL = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            adjL.add(new ArrayList<>());
+        }
+        for(int[] edge : invocations){
+            adjL.get(edge[0]).add(edge[1]);
+        }
+
+        boolean[] isSuspicious = new boolean[n];
+        Deque <Integer> stack = new ArrayDeque<>();
+        stack.push(k);
+        isSuspicious[k] = true;
+        while(!stack.isEmpty()){
+            int currentMethod = stack.pop();
+            for(int calledMethod : adjL.get(currentMethod)){
+                if(!isSuspicious[calledMethod]){
+                    isSuspicious[calledMethod] = true;
+                    stack.push(calledMethod);
+                }
+            }
+        }
+
+        for(int[] edge:invocations){
+            int caller = edge[0],called=edge[1];
+            if(!isSuspicious[caller] && isSuspicious[called]){
+                List all = new ArrayList<>();
+                for(int m = 0; m<n; m++){
+                    all.add(m);
+                }
+                return all;
+            }
+        }
+
+        List result = new ArrayList<>();
+        for(int m=0; m<n; m++){
+            if(!isSuspicious[m]) result.add(m);
+        }
+        return result;
+        
+    }
+}
